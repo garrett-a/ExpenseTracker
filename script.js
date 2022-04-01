c53bfa7ddb62a0170dfa259231069bbdbc7ad24a
garrett-a <94200645+garrett-a@users.noreply.github.com>
2022-04-01 14:04:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ const noExpenseRow = document.querySelector(".no-expense");
 const deleteRow = document.querySelector(".delete-row");
 const tableBody = document.querySelector("tbody");
 
-let expenses = [{ id: "", name: "", date: "", category: "", amount: "" }];
+let expenses = [];
 
-// MAKE BUTTON DO NOTHING IF EMPTY
 addBtn.addEventListener("click", function () {
   if (nameInput.value && dateInput.value && amountInput.value) {
     addExpenseToCategory(categoryInput.value, amountInput.value);
@@ -55,7 +54,8 @@ function addExpense() {
     tableDataAmount.innerHTML = `$${item.amount}`;
 
     xmarkIcon.addEventListener("click", function () {
-      removeExpenseToCategory(index);
+      removeExpense(item.id);
+      removeExpenseToCategory(item.category, item.amount);
       tableRow.remove();
       xmarkIcon.remove();
       console.log(expenses);
@@ -77,52 +77,11 @@ function addExpense() {
     noExpenseRow.style.display = "none";
   });
 }
-// // ADD USER INPUT TO THE DOM
-// function addExpense() {
-//   const tableRow = document.createElement("tr");
-//   tableRow.classList = "input-table-row";
-//   const tableDataName = document.createElement("td");
-//   const tableDataDate = document.createElement("td");
-//   const tableDataCategory = document.createElement("td");
-//   const tableDataAmount = document.createElement("td");
-//   const xmarkIcon = document.createElement("i");
-
-//   // Insert input values
-//   tableDataName.innerHTML = nameInput.value;
-//   tableRow.appendChild(tableDataName);
-
-//   tableDataDate.innerHTML = dateInput.value;
-//   tableRow.appendChild(tableDataDate);
-
-//   tableDataCategory.innerHTML = categoryInput.value;
-//   tableRow.appendChild(tableDataCategory);
-
-//   tableDataAmount.innerHTML = `$${amountInput.value}`;
-//   tableRow.appendChild(tableDataAmount);
-
-//   xmarkIcon.className = "fa-solid fa-xmark";
-//   tableRow.appendChild(xmarkIcon);
-
-//   xmarkIcon.addEventListener("click", function () {
-//     removeExpenseToCategory(this.id);
-//     tableRow.remove();
-//     xmarkIcon.remove();
-//     console.log(expenses);
-//   });
-
-//   table.appendChild(tableRow);
-
-//   // Clear all fields
-//   nameInput.value = "";
-//   dateInput.value = "";
-//   amountInput.value = "";
-//   noExpenseRow.style.display = "none";
-// }
 
 //TODO:
-// make x remove value from expenses
 // stop th from shifting
 // add pagination
+// local storage
 
 //////////////////////////////
 // CHART
@@ -144,13 +103,13 @@ const myChart = new Chart(ctx, {
         label: "Categories",
         data: [0, 0, 0, 0, 0, 0, 0],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(2, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.7)",
+          "rgba(54, 162, 235, 0.7)",
+          "rgba(255, 206, 86, 0.7)",
+          "rgba(75, 192, 192, 0.7)",
+          "rgba(153, 102, 255, 0.7)",
+          "rgba(255, 159, 64, 0.7)",
+          "rgba(2, 159, 64, 0.7)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
@@ -170,7 +129,7 @@ const myChart = new Chart(ctx, {
   },
 });
 
-//// ADD USER INPUT TO CHART
+// ADD USER INPUT TO CHART
 const addExpenseToCategory = (category, amount) => {
   const lowercaseLabels = myChart.data.labels.map((l) => l.toLowerCase());
   const index = lowercaseLabels.indexOf(category.toLowerCase());
@@ -182,14 +141,17 @@ const addExpenseToCategory = (category, amount) => {
   }
 };
 
-//// REMOVE USER INPUT FROM CHART
-const removeExpenseToCategory = (index) => {
-  expenses = expenses.filter((expense, index) => expense.index !== index);
+// REMOVE USER INPUT FROM CHART
+const removeExpenseToCategory = (category, amount) => {
+  const lowercaseLabels = myChart.data.labels.map((l) => l.toLowerCase());
+  const index = lowercaseLabels.indexOf(category.toLowerCase());
+
+  if (index !== -1) {
+    myChart.data.datasets[0].data[index] -= parseInt(amount);
+    myChart.update();
+    console.log(myChart.data.datasets[0].data);
+  }
 };
-// //// REMOVE USER INPUT FROM CHART
-// const removeExpenseToCategory = (expense) => {
-//   expenses = expenses.filter((expense) => expense.index !== index);
-// };
 
 // PUSH INPUT TO EXPENSES OBJECT
 const pushExpense = function () {
@@ -202,4 +164,9 @@ const pushExpense = function () {
   };
   return expenses.push(expense);
   console.log(expenses);
+};
+
+// REMOVE EXPENSE FROM ARRAY
+const removeExpense = (id) => {
+  expenses = expenses.filter((expense) => expense.id !== id);
 };
